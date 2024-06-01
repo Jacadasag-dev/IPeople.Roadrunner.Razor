ca11ee77ba49b5d07fc47a18e2c4c77254c53a0a
Chase Armfield <Jacadasag@gmail.com>
2024-05-31 21:21:28 -0500

    Added service collection extensions

---
+++ ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace IPeople.Roadrunner.Razor
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddStateServices(this IServiceCollection services)
+        {
+            // Register any services required by your NuGet package components
+            // Example:
+            // services.AddSingleton<YourService>();
+
+            services.AddSingleton<Shared.RrServices.ViewStateService>();
+            services.AddSingleton<Shared.RrServices.InputStateService>();
+            services.AddSingleton<Shared.RrServices.DropdownStateService>();
+            services.AddSingleton<Shared.RrServices.ErrorLoggingService>();
+            return services;
+        }
+    }
+}
